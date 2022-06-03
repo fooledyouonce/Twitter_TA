4701f6e1367593b111b925c0a3fede622eb12afa
Emily Crowl <emilycrowl@fb.com>
2022-06-02 23:14:50 -0700

    started compose fragment

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/FragmentCompose.java
@@ -0,0 +1,145 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.codepath.apps.restclienttemplate.databinding.ActivityComposeBinding;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+
+import java.util.Objects;
+
+import okhttp3.Headers;
+
+//TODO: Edit TimeActivity to work with ComposeFragment
+public class ComposeFragment extends DialogFragment implements TextView.OnEditorActionListener {
+
+    public static final String TAG = "ComposeActivity";
+    public static final int MAX_TWEET_LENGTH = 280;
+    EditText etCompose;
+    Button btnTweet;
+    TwitterClient client;
+    ActivityComposeBinding binding;
+    Tweet replyToTweet;
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) { return false; }
+
+    public interface TweetListener { void onTweetButton(Tweet tweet); }
+
+    public ComposeFragment() {
+        // Empty constructor is required for DialogFragment
+        // Make sure not to add arguments to the constructor
+        // Use `newInstance` instead as shown below
+    }
+
+    public static ComposeFragment newInstance(String username) {
+        ComposeFragment frag = new ComposeFragment();
+        Bundle args = new Bundle();
+        args.putString("username", username);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = ActivityComposeBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        etCompose = binding.etCompose;
+        btnTweet = binding.btnTweet;
+        client = TwitterApp.getRestClient(getContext());
+
+        etCompose.setText(getArguments().getString("username"));
+
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tweetContent = etCompose.getText().toString();
+                if (tweetContent.isEmpty()) {
+                    Toast.makeText(getActivity(), "Your tweet cannot be empty!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (tweetContent.length() > MAX_TWEET_LENGTH) {
+                    Toast.makeText(getActivity(), "Your tweet must be less than 280 characters!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (replyToTweet != null) {
+                    if (!tweetContent.substring(0, replyToTweet.user.screenName.length() + 1).equals("@" + replyToTweet.user.screenName)) {
+                        Toast.makeText(getActivity(), "Include user!", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    else {
+                        client.replyToTweet(tweetContent, replyToTweet.tweetId, new JsonHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                                try {
+                                    Tweet tweet = Tweet.fromJson(json.jsonObject);
+                                    TweetListener listener = (TweetListener) getActivity();
+                                    assert listener != null;
+                                    listener.onTweetButton(tweet);
+                                    dismiss();
+                                } catch (JSONException e) {
+                                    Log.e(TAG, "Json exception");
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {}
+                        });
+                    }
+                    etCompose.requestFocus();
+                }
+
+                //make api call to twitter to publish tweet
+                //Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show(); //debugging
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            TweetListener listener = (TweetListener) getActivity();
+                            listener.onTweetButton(tweet);
+                            dismiss();
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Json exception");
+                        }
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) { Log.e(TAG, "onFailure", throwable); }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        Objects.requireNonNull(getDialog()).getWindow().setLayout((6 * width)/7, (3 * height)/5);
+        super.onStart();
+
+    }
+}
